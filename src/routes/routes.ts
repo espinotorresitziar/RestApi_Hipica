182e70bc1ac8b874d165feac03ddafa7525f30c2
ItziarEspino <itziaespino19@iespuntadelverde.es>
2022-04-29 09:15:39 +0200

    first commit

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -256,7 +256,7 @@ class DatoRoutes {
     misRutas(){
         this._router.get('/niveles', this.getNiveles)
         this._router.get('/participantes', this.getParticipantes)
-        this._router.get('/niveles/:tipoNivel', this.getNivel)
+        this._router.get('/niveles/:_tipoNivel', this.getNivel)
         this._router.get('/participante/:nombre', this.getParticipante)
         this._router.post('/nivel', this.newNivel)
         this._router.post('/participante', this.newParticipante)
