01941208c926822f12c05d10453d5e969b0458a7
ItziarEspino <itziaespino19@iespuntadelverde.es>
2022-05-06 09:04:01 +0200

    first commit

--- build/routes/routes.js
+++ build/routes/routes.js
@@ -50,11 +50,11 @@ class DatoRoutes {
             database_1.db.desconectarBD();
         });
         this.getNivel = (req, res) => __awaiter(this, void 0, void 0, function* () {
-            const { tipoNivel } = req.params;
+            const { _tipoNivel } = req.params;
             yield database_1.db.conectarBD()
                 .then(() => __awaiter(this, void 0, void 0, function* () {
                 const query = yield niveles_1.Niveles.findOne({
-                    '_tipoNivel': tipoNivel
+                    '_tipoNivel': _tipoNivel
                 });
                 /*const query = await Niveles.aggregate(
                     [
@@ -231,7 +231,7 @@ class DatoRoutes {
     misRutas() {
         this._router.get('/niveles', this.getNiveles);
         this._router.get('/participantes', this.getParticipantes);
-        this._router.get('/niveles/:tipoNivel', this.getNivel);
+        this._router.get('/niveles/:_tipoNivel', this.getNivel);
         this._router.get('/participante/:nombre', this.getParticipante);
         this._router.post('/nivel', this.newNivel);
         this._router.post('/participante', this.newParticipante);
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -53,12 +53,12 @@ class DatoRoutes {
     }
 
     private getNivel = async (req: Request, res: Response) => {
-        const { tipoNivel } = req.params
+        const { _tipoNivel } = req.params
         await db.conectarBD()
         .then (async () => {
             const query = await Niveles.findOne(
                 {
-                    '_tipoNivel': tipoNivel
+                    '_tipoNivel': _tipoNivel
                 }
             )
             /*const query = await Niveles.aggregate(
@@ -256,7 +256,7 @@ class DatoRoutes {
     misRutas(){
         this._router.get('/niveles', this.getNiveles)
         this._router.get('/participantes', this.getParticipantes)
-        this._router.get('/niveles/:tipoNivel', this.getNivel)
+        this._router.get('/niveles/:_tipoNivel', this.getNivel)
         this._router.get('/participante/:nombre', this.getParticipante)
         this._router.post('/nivel', this.newNivel)
         this._router.post('/participante', this.newParticipante)
