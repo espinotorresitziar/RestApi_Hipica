53b3aa48abf41261d27d92fbea9f3745c9fde918
ItziarEspino <itziaespino19@iespuntadelverde.es>
2022-05-10 14:43:16 +0200

    first commit

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -53,12 +53,12 @@ class DatoRoutes {
     }
 
     private getNivel = async (req: Request, res: Response) => {
-        const { id } = req.params
+        const { tipoNivel } = req.params
         await db.conectarBD()
         .then (async () => {
             const query = await Niveles.findOne(
                 {
-                    '_id': id
+                    '_tipoNivel': tipoNivel
                 }
             )
             /*const query = await Niveles.aggregate(
@@ -256,7 +256,7 @@ class DatoRoutes {
     misRutas(){
         this._router.get('/niveles', this.getNiveles)
         this._router.get('/participantes', this.getParticipantes)
-        this._router.get('/niveles/:id', this.getNivel)
+        this._router.get('/niveles/:tipoNivel', this.getNivel)
         this._router.get('/participante/:nombre', this.getParticipante)
         this._router.post('/nivel', this.newNivel)
         this._router.post('/participante', this.newParticipante)
